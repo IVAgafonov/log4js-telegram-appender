eb0e62bd647a2c77b1b45ad6b231f3cbcbba0bab
Igor Agafonov <igoradm90@gmail.com>
2022-06-27 20:47:29 +0300

    add levels

--- index.js
+++ index.js
@@ -3,16 +3,21 @@ const os = require("os");
 
 const telegramAppender = {
     configure(config) {
+        let link = null;
+        const source = config.source + '@' + os.hostname();
+        if (config.link) {
+            link = config.link.replaceAll('{{source}}', source);
+        }
         return (loggingEvent) => {
-            if (loggingEvent.level.level !== 40000) {
+            if (config.levels && !config.levels.includes(loggingEvent.level.levelStr)) {
                 return;
             }
-            const source = config.source + '@' + os.hostname();
+
             let message = `Error from: <i>${source}</i>:
 ${loggingEvent.data[0].slice(0, 255)}
 `;
-            if (config.link) {
-                message += `<a href="${config.link}source%3A\\` + source + `\+AND+level%3A+\ERROR\&rangetype=relative&from=18000">link</a>`;
+            if (link) {
+                message += `<a href="${link}">link</a>`;
             }
             const params = {
                 chat_id: config.chatId,
