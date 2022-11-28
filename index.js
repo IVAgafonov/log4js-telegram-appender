f9488acebaff6cd4c25d59a9a7c22acdefcc0504
iagafonov <igoradm90@gmail.com>
2022-11-28 11:05:00 +0400

    add message trottle

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
 const axios = require("axios");
 const os = require("os");
 
+const msgHM = new Map();
+
 const telegramAppender = {
     configure(config) {
         let link = null;
         const source = config.source + '@' + os.hostname();
+        const ttlCache = config.ttl || 60;
+
+        setInterval(() => {
+            for (const m of msgHM.entries()) {
+                if (m[1] < new Date - ttlCache * 60 * 1000) {
+                    msgHM.delete(m[0]);
+                }
+            }
+        }, 10 * 60 * 1000);
+
         if (config.link) {
             link = config.link.replaceAll('{{source}}', source);
         }
         return (loggingEvent) => {
+            const textMessage = loggingEvent.data[0].slice(0, 255);
+
+            if (msgHM.has(textMessage)) {
+                console.log(`Message was sent recently`);
+                return;
+            }
+            msgHM.set(textMessage, new Date())
+
             if (config.levels && !config.levels.includes(loggingEvent.level.levelStr)) {
                 return;
             }
-
             let message = `Error from: <i>${source}</i>:
-${loggingEvent.data[0].slice(0, 255)}
+${textMessage}
 `;
             if (link) {
                 message += `<a href="${link}">link</a>`;
